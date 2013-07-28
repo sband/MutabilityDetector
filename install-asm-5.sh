3d8212a4ece39cf4103a19ca059d9d54b8b2bb54
Grundlefleck <grundlefleck@gmail.com>
2013-07-28 20:22:31 +0100

    Fix script to try to resolve pom file from correct location.

--- install-asm-5.sh
+++ install-asm-5.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+#! /bin/sh -x
 
-mkdir /tmp/install_asm
+mkdir -p /tmp/install_asm
 cd /tmp/install_asm
 wget http://download.forge.objectweb.org/asm/asm-5.0_ALPHA-bin.zip
 unzip asm-5.0_ALPHA-bin.zip
-mvn install -f /tmp/asm-5.0_ALPHA/lib/asm-parent-5.0_ALPHA.pom
+mvn install -f asm-5.0_ALPHA/lib/asm-parent-5.0_ALPHA.pom
 mvn install:install-file -Dfile=asm-5.0_ALPHA/lib/all/asm-debug-all-5.0_ALPHA.jar -DpomFile=asm-5.0_ALPHA/lib/all/asm-debug-all-5.0_ALPHA.pom
 
 
